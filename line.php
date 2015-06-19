44ff73a89a772582bd5fe6aeb2ca7ff3c7322fce
James <jmp464@cornell.edu>
2015-06-19 11:03:51 -0400

    line.php has a form that can choose the relevant years

--- line.php
+++ line.php
@@ -3,37 +3,54 @@ include 'session.php';
 include 'common.php';
 //get # of rows in each table to pass to browsing pages.
 
-
-$datearr = array(
-      2000 => 0,
-      2001 => 0,
-      2002 => 0,
-      2003 => 0,
-      2004 => 0,
-      2005 => 0,
-      2006 => 0,
-      2007 => 0,
-      2008 => 0,
-      2009 => 0,
-      2010 => 0,
-      2011 => 0,
-      2012 => 0,
-      2013 => 0,
-      2014 => 0,
-      2015 => 0
-
-    );
-$sql="SELECT crime_classification, news_date FROM cases  ORDER BY news_date";
-foreach ($db->query($sql) as $data){
-
-  $date = htmlspecialchars($data['news_date']);
-  $date=strval($date);
-  $date=strtotime($date);
-  $date = idate('Y', $date);
-  
-  $datearr[$date]++;
-
+//initialize an array with zero values for all the years in question.
+if (isset($_POST['submit'])) {
+  $begin=$_POST['begin'];
+  $end=$_POST['end'];
 }
+else{
+$begin=2003;
+$end=2015;
+}
+$datearr = array();
+for ($i=$begin; $i <= $end ; $i++) { 
+  $datearr[$i]=0;
+}
+
+//get the relevant data from the table
+$sql=$db->prepare("SELECT crime_classification, news_date FROM cases  ORDER BY news_date");
+
+  //these variables are going to be passed to a form to dynamically generate the first and last relevant years.
+  //Note if it turns out cybercrim happened before 0 b.c. or after 3000, then these constants will need to be changed.
+  $firstyear=3000;
+  $lastyear=0;
+if ($sql->execute()) {
+  while($data=$sql->fetch(PDO::FETCH_ASSOC)){
+
+    $date = htmlspecialchars($data['news_date']);
+    $date=strval($date);
+    $date=strtotime($date);
+    $date = idate('Y', $date);
+
+    if(isset($datearr[$date])){
+    $datearr[$date]++;
+  }
+
+  if($date<$firstyear){
+    $firstyear=$date;
+  }
+  if($date>$lastyear){
+    $lastyear=$date;
+  }
+
+  }
+}
+$yeardropdown="";
+for ($i=$firstyear; $i<=$lastyear; $i++) { 
+  $yeardropdown.="<option value=$i>$i</option>";
+}
+
+//create a string full of data points for the chart
 $graphdata="";
 foreach ($datearr as $year => $count) {
   
@@ -91,11 +108,20 @@ function drawBasic() {
 
   <div id="content">
     <h2>FBI Reported Cyber Crime Cases 2000-2015</h2>
-      <div id="chart_div"><div>
+      <div id="chart_div"></div>
 	
+  <form method='post' action='line.php'>
+    Start Year
+    <select name='begin'><?php echo $yeardropdown; ?></select><br>
+    End Year
+    <select name='end'><?php echo $yeardropdown; ?></select><br>
+    <input type='submit' name='submit'></input>
+  </form>
+</div>
+<?php include 'footer.php' ?>
 </div>
 </body>
 </html>
 
-<script src="js/hilight.js" type="text/javascript"></script>
+<script src="js/hilightservice.js" type="text/javascript"></script>
 
